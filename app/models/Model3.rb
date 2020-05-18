e73faffe220fa9cdb75610626f8cafcd1702600f
callas4ever <thetseringnorbu@gmail.com>
2020-05-17 23:24:18 -0400

    glad

--- app/models/Model3.rb
+++ app/models/Model3.rb
@@ -1,5 +1,17 @@
 #class for Model3 goes here
 #Feel free to change the name of the class
-class Model3
+class Subscriber
+    @@all=[]
+    attr_accessor :name, :age
+    def initialize(name, age)
+        @name=name
+        @age=age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
   
 end
